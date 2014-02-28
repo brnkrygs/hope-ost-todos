3eee933fe95f830001edf77c5b363e794ac82417
Brian Krygsman <bkrygsman@ostusa.com>
2014-02-27 22:24:03 -0500

    Drop DotNetOpenAuth dependency.

--- src/Presentation.Web/App_Start/AuthConfig.cs
+++ src/Presentation.Web/App_Start/AuthConfig.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Microsoft.Web.WebPages.OAuth;
-using Presentation.Web.Models;
-
-namespace Presentation.Web
+﻿namespace Presentation.Web
 {
     public static class AuthConfig
     {
