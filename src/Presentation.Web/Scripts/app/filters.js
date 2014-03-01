b5e0af5a8d0553c89f05278e578e68ea9633a136
Brian Krygsman <bkrygsman@ostusa.com>
2014-02-28 21:31:34 -0500

    Implement reverse filter, apply to list titles.

--- src/Presentation.Web/Scripts/app/filters.js
+++ src/Presentation.Web/Scripts/app/filters.js
@@ -1,21 +1,30 @@
 ﻿// Put together some filters for acting on Todo items
-angular.module("todos.filters", []).
+angular.module( "todos.filters", [] ).
 
     // Filter to return todo items that are incomplete
     // Uses Angular's built-in filter function
-    filter('incomplete', function() {
-        return function(todos) {
-            return todos.filter(function(todo) {
+    filter( 'incomplete', function () {
+        return function ( todos ) {
+            return todos.filter( function ( todo ) {
                 return todo.Completed === false;
-            });
+            } );
         };
-    }).
+    } ).
 
     // Filter to return todo items that are complete
-    filter('complete', function() {
-        return function(todos) {
-            return todos.filter(function(todo) {
+    filter( 'complete', function () {
+        return function ( todos ) {
+            return todos.filter( function ( todo ) {
                 return todo.Completed === true;
-            });
+            } );
         };
-    });
+    } ).
+
+    filter( 'reverse', function () {
+        return function ( target ) {
+
+            var reversed = target.split( "" ).reverse().join('');
+            return reversed;
+
+        };
+    } );
